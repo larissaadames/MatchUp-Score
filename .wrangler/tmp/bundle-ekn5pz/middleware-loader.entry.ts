0ec4e8b1d7262c69e208f1b4c26ceaf99ac54c55
Larissa Adames <larissaadames0@gmail.com>
2025-06-05 00:46:44 -0300

    header geral e modificacoes da nav

--- .wrangler/tmp/bundle-ekn5pz/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-ekn5pz/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\laris\\OneDrive\\Documentos\\GitHub\\MatchUp-Score\\.wrangler\\tmp\\bundle-EP70RY\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "C:\\Users\\laris\\OneDrive\\Documentos\\GitHub\\MatchUp-Score\\.wrangler\\tmp\\bundle-ekn5pz\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\laris\\AppData\\Roaming\\npm\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "C:\\Users\\laris\\OneDrive\\Documentos\\GitHub\\MatchUp-Score\\.wrangler\\tmp\\bundle-EP70RY\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "C:\\Users\\laris\\OneDrive\\Documentos\\GitHub\\MatchUp-Score\\.wrangler\\tmp\\bundle-ekn5pz\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "C:\\Users\\laris\\OneDrive\\Documentos\\GitHub\\MatchUp-Score\\.wrangler\\tmp\\bundle-EP70RY\\middleware-insertion-facade.js";
+export * from "C:\\Users\\laris\\OneDrive\\Documentos\\GitHub\\MatchUp-Score\\.wrangler\\tmp\\bundle-ekn5pz\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
